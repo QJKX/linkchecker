b587fdde4167ab65b346f164bd4edc86a797d53d
cjmayo <cjmayo@users.noreply.github.com>
2022-08-30 18:28:16 +0000

    Merge pull request #632 from cjmayo/docs
    Assorted documentation updates d72649453cf51fba5421ad66fc273c11d7a0aa24

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post45+gffe1be37',
+    VERSION: '10.1.0.post50+gd7264945',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
