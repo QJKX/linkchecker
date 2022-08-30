2b91a579d3df4ccfd6ab02b4fa20250a6048d473
cjmayo <cjmayo@users.noreply.github.com>
2022-08-30 18:29:06 +0000

    Merge pull request #633 from cjmayo/ldap
    Re-enable ldap: test 72310d4e0d2415d85a33ad096732a09417fcd671

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post50+gd7264945',
+    VERSION: '10.1.0.post52+g72310d4e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
