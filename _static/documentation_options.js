84d7ba4e239c2273dc1b7031b6294c26b2158265
cjmayo <cjmayo@users.noreply.github.com>
2022-08-30 18:29:13 +0000

    Merge pull request #634 from cjmayo/pyxdg
    Remove dependency on pyxdg b35036af2b11305d306072ec030a0a7be4a01d72

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post52+g72310d4e',
+    VERSION: '10.1.0.post54+gb35036af',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
