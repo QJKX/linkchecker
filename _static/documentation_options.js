d415c3ee4c70184c1ddc567bda0e7a9ac5eba77a
cjmayo <cjmayo@users.noreply.github.com>
2022-09-02 18:18:38 +0000

    Merge pull request #640 from kianmeng/fix-typos
    Fix typos 6958e2e2378a7268f51c9e47ff86f607b4f59500

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post56+g75196921',
+    VERSION: '10.1.0.post58+g6958e2e2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
