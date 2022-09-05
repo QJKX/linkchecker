86c612b2280e107fe874ff78fe4ef276d706163c
cjmayo <cjmayo@users.noreply.github.com>
2022-09-05 18:29:13 +0000

    Merge pull request #639 from cjmayo/hatch
    Replace setuptools and setup.py with hatch and pyproject.toml c627b00755a93f3f4bcdb35eece79d5044e5020d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post68+gd5058ecd',
+    VERSION: '10.1.0.post74+gc627b007',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
