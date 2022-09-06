3d379c468af2898b8eea98f814969a0bfe19a449
cjmayo <cjmayo@users.noreply.github.com>
2022-09-06 18:24:28 +0000

    Merge pull request #646 from cjmayo/unidir
    Fix checking directory containing Unicode filenames 595ce32e554a28b41cf680a0baabfcd018905fa3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post74+gc627b007',
+    VERSION: '10.1.0.post77+g595ce32e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
