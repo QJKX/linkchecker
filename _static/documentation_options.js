7a1858bdf814b97f91465bb37073c32738947fc1
cjmayo <cjmayo@users.noreply.github.com>
2022-09-12 18:27:29 +0000

    Merge pull request #651 from cjmayo/rate
    Rename url-rate-limited to http-rate-limited 30e8cfad773a0172f9cf34545e12441ff80d4c59

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post93+g84443b14',
+    VERSION: '10.1.0.post95+g30e8cfad',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
