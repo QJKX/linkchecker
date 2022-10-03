c07fde508273c84bf7c0e35a30e622d7b06d7ef4
cjmayo <cjmayo@users.noreply.github.com>
2022-10-03 18:23:58 +0000

    Merge pull request #671 from cjmayo/example
    Fix formatting of ignoreerrors example in linkcheckerrc(5) 033dcf89f96bd9776b2e39bed6ca01e2ea331eb2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post116+gd6d5e918',
+    VERSION: '10.1.0.post118+g033dcf89',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
