104f1ddd53549695bf362b78597866ae4076b7d0
cjmayo <cjmayo@users.noreply.github.com>
2022-10-03 18:23:26 +0000

    Merge pull request #672 from cjmayo/encoding
    Separate URL encoding and content encoding d6d5e918dc75ceeffe1f5ce7031eafcbdef689d0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post114+g61071fc5',
+    VERSION: '10.1.0.post116+gd6d5e918',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
