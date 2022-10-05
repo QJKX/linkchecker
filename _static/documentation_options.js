bde63d626bf8deab6a1513d5a83e18e973ae2083
cjmayo <cjmayo@users.noreply.github.com>
2022-10-05 18:22:34 +0000

    Merge pull request #673 from cjmayo/sitemap
    Fix sitemap output with multiple threads 354ea933ca2b40a68fb1b9ec8f668ee28f172f67

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post129+gd9265bb7',
+    VERSION: '10.1.0.post133+g354ea933',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
