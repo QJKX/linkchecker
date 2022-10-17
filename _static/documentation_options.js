ae92d9406394034b5d9d1b65609860ce50023f75
cjmayo <cjmayo@users.noreply.github.com>
2022-10-17 18:27:17 +0000

    Merge pull request #676 from cjmayo/robotmap
    Document sitemaps in linkchecker(1) b6eea83f632e6c7f568377470626bbbf2b80c8fb

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post139+g96c33360',
+    VERSION: '10.1.0.post145+gb6eea83f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
