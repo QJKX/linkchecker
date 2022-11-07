9c1d75876dc9503e4048a6a4402b6929b662c2b3
cjmayo <cjmayo@users.noreply.github.com>
2022-11-07 19:23:41 +0000

    Merge pull request #689 from cjmayo/html
    Turn all warnings into errors when building website ea2cb6c59746760fd601544b1609f8019ff9273e

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post168+g353909ba',
+    VERSION: '10.1.0.post172+gea2cb6c5',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
