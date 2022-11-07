949fe4e248dd2867c236c419d371c76e4d3e33f9
cjmayo <cjmayo@users.noreply.github.com>
2022-11-07 19:22:33 +0000

    Merge pull request #688 from cjmayo/de
    Some German application translations 353909bacc50be3c19f08f2e1a4282fbb011981a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post166+gd8b6d777',
+    VERSION: '10.1.0.post168+g353909ba',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
