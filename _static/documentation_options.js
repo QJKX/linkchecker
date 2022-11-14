b852d692fd3f9b642277a99f10c11df008550f95
cjmayo <cjmayo@users.noreply.github.com>
2022-11-14 19:30:08 +0000

    Merge pull request #690 from cjmayo/pyupgrade37
    pyupgrade --py37-plus f0c03b129a37f27a87618a3864510414cc1266a2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.1.0.post172+gea2cb6c5',
+    VERSION: '10.1.0.post179+gf0c03b12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
