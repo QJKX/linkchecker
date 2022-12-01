bb1f9b9d2d4ff0c00bf8884cfb2b6af928953342
cjmayo <cjmayo@users.noreply.github.com>
2022-12-01 19:24:09 +0000

    Merge pull request #700 from cjmayo/bs4-4.11
    Fix minimum bs4 is 4.11 and test minimum requirements d90d4e1f1729025b934b97b6ebf94c3b3a3e0917

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.0',
+    VERSION: '10.2.0.post7+gd90d4e1f1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
