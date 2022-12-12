51ce45f754622e4ce99b2f9c3f9c92e02faa2643
cjmayo <cjmayo@users.noreply.github.com>
2022-12-12 19:26:13 +0000

    Merge pull request #708 from cjmayo/docker311
    Update Docker image to Python 3.11 9a5f914019f1fd61e41576f5e31ade46bb277dec

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1',
+    VERSION: '10.2.1.post2+g9a5f91401',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
