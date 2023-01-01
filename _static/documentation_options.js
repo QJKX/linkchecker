bcdc453a55ade572d15e4f7cb3364308dcd3caba
mgedmin <mgedmin@users.noreply.github.com>
2023-01-01 10:16:55 +0000

    Merge pull request #710 from linkchecker/clamav-cdn-relief
    Cache ClamAV signatures d179b916b5183e91c05dd3b67dd01cf17f19e7fe

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1.post2+g9a5f91401',
+    VERSION: '10.2.1.post4+gd179b916b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
