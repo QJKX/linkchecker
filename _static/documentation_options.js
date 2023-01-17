456437b7381c5b49f85f8c359dd38989f2c49448
cjmayo <cjmayo@users.noreply.github.com>
2023-01-17 19:58:31 +0000

    Merge pull request #719 from cjmayo/jquery
    Ensure jQuery is installed to support Sphinx 6 0a5170286b638c275ca3257cf7800fa74eb9fa90

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1.post8+gec9386d00',
+    VERSION: '10.2.1.post10+g0a5170286',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
