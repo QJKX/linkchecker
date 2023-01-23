bd73d12b5d93f912cdefb8701c6a686287163983
cjmayo <cjmayo@users.noreply.github.com>
2023-01-23 19:29:22 +0000

    Merge pull request #712 from cjmayo/getdefaultlocale
    Replace deprecated locale.getdefaultlocale() beab10ba4f56ef0c7db4b4ddfb8454b518d82bac

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1.post14+ga61ffac5d',
+    VERSION: '10.2.1.post16+gbeab10ba4',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
