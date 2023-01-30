2b1d95822b4cab8ddcb4d389ef46c02bc24e52d0
cjmayo <cjmayo@users.noreply.github.com>
2023-01-30 19:37:53 +0000

    Merge pull request #721 from cjmayo/build
    Set permissions for build workflow c055c4f52bdefca81dc34167852e2f75652f16b1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1.post23+g91105d722',
+    VERSION: '10.2.1.post25+gc055c4f52',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
