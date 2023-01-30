2b2a525b81285e12e90f242ba7cac926937b1bfe
cjmayo <cjmayo@users.noreply.github.com>
2023-01-30 19:37:30 +0000

    Merge pull request #716 from cjmayo/py38
    Raise minimum Python version to 3.8 91105d722a83bcd363c7bd4756dbb97e9a048600

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '10.2.1.post20+g3041c8af2',
+    VERSION: '10.2.1.post23+g91105d722',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
