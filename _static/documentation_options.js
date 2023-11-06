15a7657dc9dcfc07d15d8f01476365da1e52c037
cjmayo <cjmayo@users.noreply.github.com>
2023-11-06 19:23:39 +0000

    Merge pull request #767 from cdown/cdown/2023-10-31/fifo
    Allow FIFOs to be used as config files b3b3cd52064070a45417fc6c83f7840cc4dc1ba0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '10.3.0.post12+g7f50c62b6',
+    VERSION: '10.3.0.post14+gb3b3cd520',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
